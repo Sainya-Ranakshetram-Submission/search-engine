499b5c34a0c23c3bcba6d6df67e4c0ad0c78e8e8
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-12-08 14:16:05 +0530

    update

--- crawler/crawler/spiders/konohagakure.py
+++ crawler/crawler/spiders/konohagakure.py
@@ -1,9 +1,17 @@
+from __future__ import print_function     
+from ctypes import *
+from typing import List, Union, Optional
 import scrapy
 from crawler import standalone_django
 from collections.abc import Iterable
 
-def get_all_domains() -> Iterable:
-    yield None
+lib = cdll.LoadLibrary("./subdomain_finder.so")
+lib.Add.argtypes = [c_longlong, c_longlong]
+lib.Add.restype = c_longlong
+
+def get_all_subdomains() -> Iterable:
+    for i in lib.find_subdomain(domain):
+        yield i
 
 class KonohagakureCrawler(scrapy.Spider):
     name : str = 'konohagakure'
