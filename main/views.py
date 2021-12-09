bf4cb7559faae091e178f738c50ce482510d50a5
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-12-09 20:16:43 +0530

    some updates in search backend

--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from asgiref.sync import sync_to_async
+from django.utils.html import escape, strip_tags, escapejs
 from django.views.decorators.http import require_GET
 
 @sync_to_async
 @require_GET
 def home(request):
-    request.session['recorded_keywords'] = ['hi', 'hello', 'bye']
     auto_suggestions = None
     if request.session.get('recorded_keywords'):
         auto_suggestions = request.session.get('recorded_keywords')
@@ -18,11 +18,17 @@ def home(request):
 @sync_to_async
 @require_GET   
 def search_results(request):
+    #Sanitize Data
+    query = strip_tags(escape(escapejs(request.GET.get("q"))))
+    #Add Auto Suggestions
     auto_suggestions = None
     if request.session.get('recorded_keywords'):
-        auto_suggestions = request.session.get('recorded_keywords')
+        auto_suggestions = request.session.get('recorded_keywords') + [query]
+    else:
+        auto_suggestions = [query]
+    
     return render(
         request, 
         'results.html',
-        {'auto_suggestions', auto_suggestions}
+        {'auto_suggestions': auto_suggestions}
     )
